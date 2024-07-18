ceb0f0679e517c5875cbcbe75d81ef84b499d4ed
Wahieb Talal <49795325+wahebtalal@users.noreply.github.com>
2024-07-18 05:29:17 +0300

    Update __init__.py
    1

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -4,7 +4,7 @@ from decouple import config
 
 api = 19663899
 hash = "af0b19d19293e57b1b74cabcf6dcbbd6"
-bot = "5029205621:AAGWw2fkjLpLOT1m2nXVNB-rPoEgJe0Q8T0"
+bot = "1985540606:AAFn-VKlS7udRGTDkmDxGLgJdCtykq-CjVQ"
 owner = str("675889933 406793949 506779415").split()
 group = "-1001644715256"
 block = "1 2 3"
